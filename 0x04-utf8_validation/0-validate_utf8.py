d3d7a388b0dd32298bd28a553a5bc16b69bc7401
Temiloluwa22 <113702411+Temiloluwa22@users.noreply.github.com>
2024-02-01 22:16:01 +0100

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,42 +1,28 @@
 #!/usr/bin/python3
 """
-UTF-8 validation module
+UTF-8
 """
+
 
 def validUTF8(data):
     """
-    Determines if a given data set represents a valid UTF-8 encoding.
+    data: integers
+    Return: True if data is a valid,else return False
     """
-    num_bytes = 0
+    byte_count = 0
 
-    for byte in data:
-        # Check if the most significant bit is 0 or 1
-        if num_bytes == 0:
-            if byte >> 7 == 0:
-                continue
-            elif byte >> 5 == 0b110:
-                num_bytes = 1
-            elif byte >> 4 == 0b1110:
-                num_bytes = 2
-            elif byte >> 3 == 0b11110:
-                num_bytes = 3
-            else:
+    for i in data:
+        if byte_count == 0:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                byte_count = 1
+            elif i >> 4 == 0b1110:
+                byte_count = 2
+            elif i >> 3 == 0b11110:
+                byte_count = 3
+            elif i >> 7 == 0b1:
                 return False
         else:
-            # Check if the byte is a continuation byte (starts with 10)
-            if byte >> 6 != 0b10:
+            if i >> 6 != 0b10:
                 return False
-            num_bytes -= 1
-
-    return num_bytes == 0
-
-# Example usage
-data = [65]
-print(validUTF8(data))
-
-data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
-print(validUTF8(data))
-
-data = [229, 65, 127, 256]
-print(validUTF8(data))
-  
+            byte_count -= 1
+    return byte_count == 0
